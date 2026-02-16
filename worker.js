e5d0dce4a9b34746d2c59ac35ee7186687a1c232
Jatin Singh <jatin.singh@brevistay.com>
2026-02-17 01:56:16 +0530

    Used spawn child process to test the queue and worker flow

--- worker.js
+++ worker.js
@@ -1,19 +1,50 @@
-const { Worker } = require("bullmq");
-const { connection } = require("./queue");
+const { Worker } = require('bullmq');
+const { spawn } = require('child_process');
+const { connection } = require('./queue');
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
 
-const worker = new Worker('codeJobQueue', async job => {
-    console.log("Received the job : ", job.id);
-    console.log("Job Data : ", job.data);
+const worker = new Worker(
+  'codeJobQueue',
+  async job => {
 
-    return {
-        code: job.data.code
-    }
-}, { connection });
+    const tempFile = path.join(os.tmpdir(), `code-${Date.now()}.js`);
+    fs.writeFileSync(tempFile, job.data.code);
 
-worker.on('completed', job => {
-    console.log("Completed Job : ", job.id);
+    return new Promise((resolve, reject) => {
+
+      const child = spawn(process.execPath, [tempFile]);
+
+      let output = '';
+      let errorOutput = '';
+
+      child.stdout.on('data', data => {
+        output += data.toString();
+      });
+
+      child.stderr.on('data', data => {
+        errorOutput += data.toString();
+      });
+
+      child.on('close', code => {
+        fs.unlinkSync(tempFile);
+
+        if (code === 0) {
+          resolve({ output });
+        } else {
+          reject(new Error(errorOutput));
+        }
+      });
+    });
+  },
+  { connection }
+);
+
+worker.on('completed', (job, output) => {
+    console.log("Completed Job : ", job.id, output);
 })
 
 worker.on('failed', (job, err) => {
-    console.log("Failed Job : ", job.id, " Error ", job.err);
+    console.log("Failed Job : ", job.id, " Error ", err);
 })
