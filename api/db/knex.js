0275d04e36c7eeec3631e6582811fd19d6b660ab
Jatin Singh <jatin.singh@brevistay.com>
2026-02-25 15:40:56 +0530

    Added knex connection

---
+++ api/db/knex.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+const knex = require('knex');
+const config = require('../config/knexConf');
+
+const db = knex(config[process.env.NODE_ENV || "development"]);
+
+module.exports = db;
