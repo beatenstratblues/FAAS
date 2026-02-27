6e6819574d8db231a33caadd419f4e6f31736250
Jatin Singh <jatin.singh@brevistay.com>
2026-02-27 13:55:28 +0530

    users module files added and new migration added for passwordhas column on users table

---
+++ api/modules/Users/users.repository.js
@@ -0,0 +1,8 @@
+const db = require("../../infrastructure/db/db");
+
+const UserRepository = {
+    createUser: async (email, passwordHash, apiKey) => {
+    }
+};
+
+module.exports = UserRepository;
--- api/modules/Users/usersRepository.js
+++
@@ -1,6 +0,0 @@
-const db = require('../infrastructure/db/knex');
-
-const UserRepository = {
-};
-
-module.exports = UserRepository;
