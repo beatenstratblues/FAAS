81ea780a94bc0c9bda22c55d590ad6d49fe82d6d
Jatin Singh <jatin.singh@brevistay.com>
2026-03-01 16:29:12 +0530

    global error handling added

--- api/index.js
+++ api/index.js
@@ -1,8 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const  { globalErrorHandler } = require('./shared/middleware/errorHandlingMiddleware');
+const AppError = require("./shared/utils/errors");
+const app = express();
 
 app.get('/', (req, res) => {
+  throw new AppError("This is a test error", 404);
   res.send('Hello World From Docker!')
-})
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
